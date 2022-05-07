623b0d84cfd02465fa249e4ae2fcf52b4d15a2c4
darkusha-ds <mts04@inbox.ru>
2022-05-08 00:23:29 +0500

    add home page

--- web_projects/urls.py
+++ web_projects/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-
-from django.conf import settings
-from django.conf.urls.static import static
+from web_projects.views import *
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html")),
+    path('', home.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
---
+++ web_projects/views.py
@@ -0,0 +1,3 @@
+from django.views.generic import TemplateView
+
+class home(TemplateView): template_name = 'home_page/home.html'
