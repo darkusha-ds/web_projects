13f690383acd5c1de2f8e2d4b6626e5f5c0d4382
darkusha-ds <mts04@inbox.ru>
2022-06-21 19:58:10 +0500

    upd

--- folders/projects/project_101/main.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const sidebar = document.querySelector('.sidebar');
-
-hamburger.addEventListener('click',()=>{
-    hamburger.classList.toggle('clicked');
-    sidebar.classList.toggle('show');
-})
--- folders/projects/project_91/main.js
+++
@@ -1,9 +0,0 @@
-const labels = document.querySelectorAll("label");
-
-labels.forEach((label)=>{
-		label.innerHTML = label.innerText
-		.split("")
-		.map((letter,index)=>{
-			return `<span style="transition-delay:${index*100}ms">${letter}</span>`
-		}).join("");
-})
